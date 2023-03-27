d24eaed191f605de3580ed93e1c619ebb42a46f2
RRoxxxsii <mishabur38@yandex.com>
2023-03-27 13:32:26 +0300

    Started new app named basket, added link for basket page with bootstrap

--- core/core/urls.py
+++ core/core/urls.py
@@ -20,7 +20,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('store.urls', namespace='store'))
+    path('', include('store.urls', namespace='store')),
+    path('basket/', include('basket.urls', namespace='basket')),
 ]
 
 
