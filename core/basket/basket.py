531a902f2d610149a91c7949be2ac574085657b1
RRoxxxsii <mishabur38@yandex.com>
2023-03-29 20:27:19 +0300

    Changed UI

--- core/basket/basket.py
+++ core/basket/basket.py
@@ -20,11 +20,12 @@ class Basket:
         """
         Adding and updating the users basket session data
         """
-        product_id = product.id
-        if product_id not in self.basket:
-            self.basket[product_id][qty] = qty
+        product_id = str(product.id)
+
+        if product_id in self.basket:
+            self.basket[product_id]['qty'] = qty
         else:
-            self.basket[product_id] = {'price': str(product.price), 'qty': int(qty)}
+            self.basket[product_id] = {'price': str(product.price), 'qty': qty}
         self.save()
 
     def __iter__(self):
