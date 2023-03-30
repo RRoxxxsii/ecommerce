799101542def4b8cab807930cde9b99566f96451
RRoxxxsii <mishabur38@yandex.com>
2023-03-30 14:37:01 +0300

    added an ability to log-in and log-out

--- core/account/forms.py
+++ core/account/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+
 from .models import UserBase
 
 
@@ -44,6 +46,19 @@ class RegistrationForm(forms.ModelForm):
         if UserBase.objects.filter(email=email).exists():
             raise forms.ValidationError('Please use another email, that is already taken')
         return email
+
+
+class UserLoginForm(AuthenticationForm):
+
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control mb-3', 'placeholder': 'Username', 'id': 'login-username'}))
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Password',
+            'id': 'login-pwd',
+        }
+    ))
 
 
 
