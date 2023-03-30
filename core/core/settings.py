80a957ecbca72ce79ff585e748a580b073b4eade
RRoxxxsii <mishabur38@yandex.com>
2023-03-30 13:53:01 +0300

    created user sign-up with email confirmation

--- core/core/settings.py
+++ core/core/settings.py
@@ -142,3 +142,5 @@ AUTH_USER_MODEL = 'account.UserBase'
 LOGIN_REDIRECT_URL = '/account/dashboard'
 LOGIN_URL = '/account/login/'
 
+# Email setting
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
