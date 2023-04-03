5cff27c39b834647df84a1bcd77975e629c6f4b0
RRoxxxsii <mishabur38@yandex.com>
2023-04-03 12:40:45 +0300

    made changes in database: create a new model in account model changed the templates: changed dashboard template, inserted 3 boxes with orders, login and security, and your addresses to look the page better and enhance usability

--- core/account/forms.py
+++ core/account/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, PasswordResetForm, SetPasswordForm
 
-from .models import UserBase
+from .models import Customer
 
 
 class RegistrationForm(forms.ModelForm):
@@ -25,12 +25,12 @@ class RegistrationForm(forms.ModelForm):
             {'class': 'form-control', 'placeholder': 'Repeat Password'})
 
     class Meta:
-        model = UserBase
+        model = Customer
         fields = ('user_name', 'email',)
 
     def clean_user_name(self):
         user_name = self.cleaned_data['user_name'].lower()
-        r = UserBase.objects.filter(user_name=user_name)
+        r = Customer.objects.filter(user_name=user_name)
         if r.count():
             raise forms.ValidationError('Username already exists.')
         return user_name
@@ -43,7 +43,7 @@ class RegistrationForm(forms.ModelForm):
 
     def clean_email(self):
         email = self.cleaned_data['email']
-        if UserBase.objects.filter(email=email).exists():
+        if Customer.objects.filter(email=email).exists():
             raise forms.ValidationError('Please use another email, that is already taken')
         return email
 
@@ -76,7 +76,7 @@ class UserEditForm(forms.ModelForm):
             attrs={'class': 'form-control mb-3', 'placeholder': 'Firstname', 'id': 'form-lastname'}))
 
     class Meta:
-        model = UserBase
+        model = Customer
         fields = ('email', 'user_name', 'first_name',)
 
     def __init__(self, *args, **kwargs):
@@ -92,7 +92,7 @@ class PwdResetForm(PasswordResetForm):
 
     def clean_email(self):
         email = self.cleaned_data['email']
-        u = UserBase.objects.filter(email=email)
+        u = Customer.objects.filter(email=email)
         if not u:
             raise forms.ValidationError(
                 'Unfortunatley we can not find that email address')
--- core/account/views.py
+++ core/account/views.py
@@ -8,7 +8,7 @@ from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 
 from .forms import RegistrationForm, UserEditForm
-from .models import UserBase
+from .models import Customer
 from .token import account_activation_token
 
 
@@ -50,7 +50,7 @@ def account_register(request):
 def account_activate(request, uidb64, token):
     try:
         uid = force_str(urlsafe_base64_decode(uidb64))
-        user = UserBase.objects.get(pk=uid)
+        user = Customer.objects.get(pk=uid)
     except(TypeError, ValueError, OverflowError, user.DoesNotExist):
         user = None
     if user is not None and account_activation_token.check_token(user, token):
@@ -78,7 +78,7 @@ def edit_details(request):
 
 @login_required
 def delete_user(request):
-    user = UserBase.objects.get(user_name=request.user)
+    user = Customer.objects.get(user_name=request.user)
     user.is_active = False
     user.save()
     logout(request)
