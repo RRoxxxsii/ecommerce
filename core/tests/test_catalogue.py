a3995bb671ca35f05b4ba849ced5308af567b592
RRoxxxsii <mishabur38@yandex.com>
2023-04-05 14:26:21 +0300

    restructured the project: deleted all unnecessary test files in each app started testing the project: installed pytest-django, set up pytest.ini file and wrote configuration in it, provided with basic tests

---
+++ core/tests/test_catalogue.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+@pytest.fixture()
+def test_fixture1():
+    print('Run each test')
+    return 1
+
+
+def test_hello_world(test_fixture1):
+    print('function_fixture1')
+    assert test_fixture1 == 1
