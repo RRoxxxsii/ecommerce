64d1be074feccde924b8045660eb4217c0f2e0c4
RRoxxxsii <mishabur38@yandex.com>
2023-04-05 19:46:30 +0300

    worked with pytest: wrote some testcode, installed coverage

--- core/tests/test_catalogue.py
+++ core/tests/test_catalogue.py
@@ -1,12 +1,24 @@
 import pytest
 
 
-@pytest.fixture()
+@pytest.fixture(scope='module')
 def test_fixture1():
     print('Run each test')
     return 1
 
 
-def test_hello_world(test_fixture1):
+def test_hello_world1(test_fixture1):
     print('function_fixture1')
     assert test_fixture1 == 1
+
+
+def test_hello_world2(test_fixture1):
+    print('function_fixture2')
+    assert test_fixture1 == 1
+
+
+@pytest.mark.skip
+def test_hello_world3(test_fixture1):
+    print('function_fixture3')
+    assert test_fixture1 == 1
+
